0211ac59d7d895d94dbc50e4703d2c8e9fc66cb9
Marcin <marcinrozkwitalski98@gmail.com>
2022-09-03 15:01:51 +0200

    Adjustment to switching scenes

--- Chess_3D/Assets/Scripts/ButtonManagerMainMenu.cs
+++ Chess_3D/Assets/Scripts/ButtonManagerMainMenu.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManagerMainMenu : MonoBehaviour
 {
 
     SceneChanger sceneChanger;
 
-    void Awake() 
-    {
-        GameObject[] buttonManagers = GameObject.FindGameObjectsWithTag("ButtonManager");
+    // void Awake() 
+    // {
+    //     GameObject[] buttonManagers = GameObject.FindGameObjectsWithTag("ButtonManager");
 
-        if (buttonManagers.Length > 1)
-        {
-            Destroy(this.gameObject);
-        }
+    //     if (buttonManagers.Length > 1)
+    //     {
+    //         Destroy(this.gameObject);
+    //     }
 
-        DontDestroyOnLoad(this.gameObject);    
-    }
+    //     DontDestroyOnLoad(this.gameObject);    
+    // }
     
     void Start()
     {
@@ -28,6 +29,66 @@ public class ButtonManagerMainMenu : MonoBehaviour
 
         sceneChanger = GameObject.Find("SceneChanger").GetComponent<SceneChanger>();
     }
+
+    // void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    // {
+    //     if(scene.name == "MainMenu")
+    //     {
+    //         if(GameObject.Find("Button01QuickStart"))
+    //         {
+    //             Button button01 = GameObject.Find("Button01QuickStart").GetComponent<Button>();
+    //             button01.onClick.AddListener(StartNormalPVP);
+    //         }
+
+    //         if(GameObject.Find("Button02SetupCustom"))
+    //         {
+    //             Button button02 = GameObject.Find("Button02SetupCustom").GetComponent<Button>();
+    //             button02.onClick.AddListener(SetupCustomPVP);
+    //         }
+
+    //         if(GameObject.Find("Button03Options"))
+    //         {
+    //             Button button03 = GameObject.Find("Button03Options").GetComponent<Button>();
+    //             button03.onClick.AddListener(Options);
+    //         }
+
+    //         if(GameObject.Find("Button04ExitGame"))
+    //         {
+    //             Button button04 = GameObject.Find("Button04ExitGame").GetComponent<Button>();
+    //             button04.onClick.AddListener(ExitGame);
+    //         }
+    //     }
+    //     else if(scene.name == "Game")
+    //     {
+    //         if(GameObject.Find("Button01Restart"))
+    //         {
+    //             Button button01 = GameObject.Find("Button01Restart").GetComponent<Button>();
+    //             button01.onClick.AddListener(StartNormalPVP);
+    //         }
+
+    //         if(GameObject.Find("Button02MainMenu"))
+    //         {
+    //             Button button02 = GameObject.Find("Button02MainMenu").GetComponent<Button>();
+    //             button02.onClick.AddListener(LoadMainMenu);
+    //         }
+    //     }
+    //     else if(scene.name == "CustomGame")
+    //     {
+    //         if(GameObject.Find("Button01MainMenu"))
+    //         {
+    //             Button button01 = GameObject.Find("Button01MainMenu").GetComponent<Button>();
+    //             button01.onClick.AddListener(LoadMainMenu);
+    //         }
+    //     }
+    //     else if(scene.name == "Options")
+    //     {
+    //         if(GameObject.Find("Button01MainMenu"))
+    //         {
+    //             Button button01 = GameObject.Find("Button01MainMenu").GetComponent<Button>();
+    //             button01.onClick.AddListener(LoadMainMenu);
+    //         }
+    //     }
+    // }
 
     public void LoadMainMenu()
     {
